d63c7dfd3cf2c25710021a275c55236507b04c10
jangxx <janscheiper95@gmail.com>
2015-11-12 21:30:08 +0100

    first version that does something

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ util.inherits(S3TC_Decoder, Transform);
 var decoders = {
     'DXT5': {
         buf_size: 16,
-        fn: function(in_buf, out_buf) {
+        fn: function(in_buf) {
+            var out_buf = new Buffer(4 * 4 * 4); //width * height * argb
+
             var alpha0 = in_buf.readUInt8(0);
             var alpha1 = in_buf.readUInt8(1);
             var a0 = in_buf.readInt16LE(2);
@@ -36,29 +38,27 @@ var decoders = {
             ];
 
             for(var i = 0; i < 16; i++) {
-                out_buf.writeUInt8(Math.round(a2value(a[i])), i+0); //alpha
-                out_buf.writeUInt8( c2value(3 & (c[Math.floor(i / 4)] >> (8-(i+1)*2)), color0.r, color1.r) , i+1); //red
-                out_buf.writeUInt8( c2value(3 & (c[Math.floor(i / 4)] >> (8-(i+1)*2)), color0.g, color1.g) , i+2); //blue
-                out_buf.writeUInt8( c2value(3 & (c[Math.floor(i / 4)] >> (8-(i+1)*2)), color0.b, color1.b), i+3); //green
+                var e = Math.floor(i / 4); //current element
 
-                console.log(out_buf);
+                //console.log(3 & (c[e] >> (8-(i+1-e*4)*2)));
+
+                out_buf.writeUInt8(c2value(3 & c[e], color0.r, color1.r) , (i*4)+0); //red
+                out_buf.writeUInt8(c2value(3 & c[e], color0.g, color1.g) , (i*4)+1); //blue
+                out_buf.writeUInt8(c2value(3 & c[e], color0.b, color1.b) , (i*4)+2); //green
+                out_buf.writeUInt8(a2value(a[i]), (i*4)+3); //alpha
+
+                c[e] = c[e] >> 2;
+                //console.log(out_buf);
             }
 
+            return out_buf;
+
             function c2value(code, color0, color1) {
-                if(color0 > color1) {
-                    switch(code) {
-                        case 0: return color0;
-                        case 1: return color1;
-                        case 2: return (2*color0 + color1) / 3;
-                        case 3: (color0 + 2*color1) / 3;
-                    }
-                } else {
-                    switch(code) {
-                        case 0: color0;
-                        case 1: color1;
-                        case 2: (color0 + color1) / 2;
-                        case 3: 0; //black
-                    }
+                switch(code) {
+                    case 0: return color0;
+                    case 1: return color1;
+                    case 2: return (color0 + color1 + 1) >> 1;
+                    case 3: return (color0 + color1 + 1) >> 1;
                 }
             }
 
@@ -73,6 +73,7 @@ var decoders = {
                         case 5: return (3*alpha0 + 4*alpha1)/7;
                         case 6: return (2*alpha0 + 5*alpha1)/7;
                         case 7: return (1*alpha0 + 6*alpha1)/7;
+                        default: console.log(code);
                     }
                 } else {
                     switch(code) {
@@ -84,10 +85,12 @@ var decoders = {
                         case 5: return (1*alpha0 + 4*alpha1)/5;
                         case 6: return 0;
                         case 7: return 1; //why, what, WHY???
+                        default: console.log(code);
                     }
                 }
             }
-        }
+        },
+        add_to_buffer: addEXTbufferToOutbuffer
     },
     'DXT3': {
         buf_size: 16,
@@ -97,8 +100,61 @@ var decoders = {
     },
     'DXT1': {
         buf_size: 8,
-        fn: function(in_buf, out_buf) {
+        fn: function(in_buf) {
+            var out_buf = new Buffer(4 * 4 * 4); //width * height * argb
 
+            var color0 = RGB565_to_RGB888(in_buf.readInt16LE(0));
+            var color1 = RGB565_to_RGB888(in_buf.readInt16LE(2));
+            var c = [in_buf.readUInt8(4), in_buf.readUInt8(5), in_buf.readUInt8(6), in_buf.readUInt8(7)];
+
+            for(var i = 0; i < 16; i++) {
+                var e = Math.floor(i / 4); //current element
+
+                out_buf.writeUInt8(c2value(3 & c[e], color0.r, color1.r) , (i*4)+0); //red
+                out_buf.writeUInt8(c2value(3 & c[e], color0.g, color1.g) , (i*4)+1); //blue
+                out_buf.writeUInt8(c2value(3 & c[e], color0.b, color1.b) , (i*4)+2); //green
+                out_buf.writeUInt8(255, (i*4)+3); //alpha
+
+                c[e] = c[e] >> 2;
+            }
+
+            return out_buf;
+
+            function c2value(code, color0, color1) {
+                if(color0 > color1) {
+                    switch(code) {
+                        case 0: return color0;
+                        case 1: return color1;
+                        case 2: return (2*color0 + color1) / 3;
+                        case 3: return (color0 + 2*color1) / 3;
+                    }
+                } else {
+                    switch(code) {
+                        case 0: return color0;
+                        case 1: return color1;
+                        case 2: return (color0 + color1) / 2;
+                        case 3: return 0; //black
+                    }
+                }
+            }
+        },
+        add_to_buffer: addEXTbufferToOutbuffer
+    },
+    'RGBA': {
+        buf_size: 4,
+        fn: function(in_buf) {
+            return in_buf;
+        },
+        add_to_buffer: function(scope, buf) {
+            buf.copy(scope.buffer, scope._width * 4 * scope._currentY + 4 * scope._currentX, 0, 4);
+            scope._currentX++;
+
+            if(scope._currentX + 1 > scope._width) {
+                scope.push(scope.buffer.slice(scope._currentY * scope._width * 4, (scope._currentY+1) * scope._width * 4)); //push out one rows
+
+                scope._currentX = 0;
+                scope._currentY++;
+            }
         }
     }
 }
@@ -117,7 +173,10 @@ function S3TC_Decoder(fmt, width, height, options) {
 }
 
 S3TC_Decoder.prototype._transform = function(chunk, encoding, done) {
-    console.log(chunk.length);
+    if(this._restChunk != undefined) {
+        chunk = Buffer.concat([this._restChunk, chunk]);
+        this._restChunk = undefined;
+    }
 
     var dec = decoders[this._fmt];
 
@@ -126,27 +185,15 @@ S3TC_Decoder.prototype._transform = function(chunk, encoding, done) {
         if(this._currentX == this._width && this._currentY == this._height) break;
         //console.log(this._currentX, this._currentY);
 
-        var buf = new Buffer(4 * 4 * 4); //width * height * argb
-
-        //console.log(pos, chunk.length, chunk.slice(pos, dec.buf_size));
-        dec.fn(chunk.slice(pos, pos + dec.buf_size), buf);
-
-        var y = this._currentY;
-        buf.copy(this.buffer, this._width * 4 * y + 4 * this._currentX, 0, 4*4);
-        y++;
-        buf.copy(this.buffer, this._width * 4 * y + 4 * this._currentX, 4*4, 4*4);
-        y++;
-        buf.copy(this.buffer, this._width * 4 * y + 4 * this._currentX, 2*4*4, 4*4);
-        y++;
-        buf.copy(this.buffer, this._width * 4 * y + 4 * this._currentX, 3*4*4, 4*4);
-
-        this._currentX += 4;
-        if(this._currentX + 4 > this._width) {
-            this.push(this.buffer.slice(this._currentY * this._width * 4, this._currentY * 4 * this._width * 4 - 1)); //push out four rows
-
-            this._currentX = 0;
-            this._currentY += 4;
+        if(chunk.length < pos + dec.buf_size) {
+            this._restChunk = new Buffer(chunk.length - pos);
+            chunk.copy(this._restChunk, 0, pos);
+            break;
         }
+
+        var buf = dec.fn(chunk.slice(pos, pos + dec.buf_size));
+
+        dec.add_to_buffer(this, buf);
 
         pos += dec.buf_size;
     }
@@ -155,6 +202,25 @@ S3TC_Decoder.prototype._transform = function(chunk, encoding, done) {
 }
 
 module.exports = S3TC_Decoder;
+
+function addEXTbufferToOutbuffer(scope, buf) {
+    var y = scope._currentY;
+    buf.copy(scope.buffer, scope._width * 4 * y + 4 * scope._currentX,     0,   4*4);
+    y++;
+    buf.copy(scope.buffer, scope._width * 4 * y + 4 * scope._currentX,   4*4, 2*4*4);
+    y++;
+    buf.copy(scope.buffer, scope._width * 4 * y + 4 * scope._currentX, 2*4*4, 3*4*4);
+    y++;
+    buf.copy(scope.buffer, scope._width * 4 * y + 4 * scope._currentX, 3*4*4, 4*4*4);
+
+    scope._currentX += 4;
+    if(scope._currentX + 4 > scope._width) {
+        scope.push(scope.buffer.slice(scope._currentY * scope._width * 4, (scope._currentY+4) * scope._width * 4)); //push out four rows
+
+        scope._currentX = 0;
+        scope._currentY += 4;
+    }
+}
 
 function RGB565_to_RGB888(rgb) {
     return {
--- test.js
+++ test.js
@@ -1,9 +1,20 @@
 var S3TC_Decoder = require('./index.js');
 var fs = require('fs');
+var PNG = require('pngjs').PNG;
 
 var instream = fs.createReadStream(process.argv[2]);
-var decoder = new S3TC_Decoder('DXT5', 1024, 1024)
+var decoder = new S3TC_Decoder('RGBA', 309, 444);
 var outstream = fs.createWriteStream(process.argv[3]);
+var outstream2 = fs.createWriteStream(process.argv[4]);
 
 instream.pipe(decoder);
-decoder.pipe(outstream);
+/*decoder.on('data', function(d) {
+    //console.log(d.toString('hex'));
+});*/
+decoder.pipe(outstream2);
+
+decoder.on('end', function() {
+    var p = new PNG({width: 309, height: 444});
+    decoder.buffer.copy(p.data);
+    p.pack().pipe(outstream);
+});
